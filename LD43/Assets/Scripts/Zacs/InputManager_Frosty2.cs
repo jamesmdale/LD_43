5f834f243a2bdc71c286e2d96f83c9c735045fcd
ZACHBRACKEN\Zach Bracken <ztbracken@gmail.com>
2018-12-01 17:35:00 -0600

    ZB - Added the new networking changed for my behavior and removed the network controller from hub scene

--- LD43/Assets/Scripts/Zacs/InputManager_Frosty2.cs
+++ LD43/Assets/Scripts/Zacs/InputManager_Frosty2.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager_Frosty2 : MonoBehaviour {
+public class InputManager_Frosty2 : InputManager {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    public override void ProcessSpace(bool isDown)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public override void ProcessShift(bool isDown)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public override void ProcessHorizontalAxis(float axis)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public override void ProcessVerticalAxis(float axis)
+    {
+        //throw new NotImplementedException();
+    }
 }
