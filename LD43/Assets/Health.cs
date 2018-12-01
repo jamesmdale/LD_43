644b2e54a12c1dce4b5422c8fc4aef2723cfe04c
Michael Dale <jmdale@smu.edu>
2018-12-01 14:29:35 -0600

    MD - health update.

--- LD43/Assets/Health.cs
+++ LD43/Assets/Health.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
 
     public const int maxHeatlh = 10;
     public int currentHealth = maxHeatlh;
+    public Text uiText;
+
+    void Start()
+    {
+        GameObject textCanvas = GameObject.Find("HealthText");
+        uiText = textCanvas.GetComponent<Text>(); 
+        
+    }
 
     public void TakeDamage(int amount)
     {
@@ -15,6 +24,9 @@ public class Health : MonoBehaviour {
             currentHealth = 0;
             Debug.Log("Player dead");
         }
+
+        if (uiText)
+            uiText.text = currentHealth.ToString();
     }
     
 }
