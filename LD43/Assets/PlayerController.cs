4d4943a3cd33761960416e8c8d97354c02f0ac25
Michael Dale <jmdale@smu.edu>
2018-12-01 10:48:36 -0600

    MD - First POCT version of networking.

---
+++ LD43/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerController : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public override void OnStartLocalPlayer()
+    {
+       GetComponent<SpriteRenderer>().material.color = Color.blue;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Translate(x, y, 0);
+	}
+}
