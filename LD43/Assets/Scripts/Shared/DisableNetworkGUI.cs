a8bf0dbf299e0b3118eb0623606369aac1bf8173
ZACHBRACKEN\Zach Bracken <ztbracken@gmail.com>
2018-12-02 14:24:49 -0600

    ZB - Turn off stuff in the hub

---
+++ LD43/Assets/Scripts/Shared/DisableNetworkGUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisableNetworkGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		NetworkManager.singleton.GetComponent<NetworkManagerHUD>().showGUI = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
