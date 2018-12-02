d40cc6bc1d132c3f40641e88e30153c13b209b5a
ZACHBRACKEN\Zach Bracken <ztbracken@gmail.com>
2018-12-01 19:22:29 -0600

    ZB - messed with movement

--- LD43/Assets/Scripts/Zacs/InputManager_Frosty2.cs
+++ LD43/Assets/Scripts/Zacs/InputManager_Frosty2.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class InputManager_Frosty2 : InputManager {
 
+    float speed = 6.0f;
+
+
 	// Use this for initialization
 	void Start ()
     {
@@ -28,11 +31,47 @@ public class InputManager_Frosty2 : InputManager {
 
     public override void ProcessHorizontalAxis(float axis)
     {
-        //throw new NotImplementedException();
+        if (axis == 0.0f)
+            return;
+
+
+        float rate = 0.0f;
+
+        if(axis < 0.0f)
+        {
+            rate = (Time.deltaTime * speed) * -1.0f;
+        }
+        else
+        {
+            rate = Time.deltaTime * speed;
+        }
+
+        
+
+        transform.Translate(rate, 0, 0);
+
     }
 
     public override void ProcessVerticalAxis(float axis)
     {
-        //throw new NotImplementedException();
+        if (axis == 0.0f)
+            return;
+
+
+        float rate = 0.0f;
+
+        if (axis < 0.0f)
+        {
+            rate = (Time.deltaTime * speed) * -1.0f;
+        }
+        else
+        {
+            rate = Time.deltaTime * speed;
+        }
+
+
+
+        transform.Translate(0, rate, 0);
+
     }
 }
