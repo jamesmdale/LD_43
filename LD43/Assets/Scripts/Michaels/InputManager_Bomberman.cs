6da7764a8dc06dda43d2bcfb7317b2fdb22c3414
Michael Dale <jmdale@smu.edu>
2018-12-02 21:22:22 -0600

    MD - add explosion prefab and bomb explosion logic.

--- LD43/Assets/Scripts/Michaels/InputManager_Bomberman.cs
+++ LD43/Assets/Scripts/Michaels/InputManager_Bomberman.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class InputManager_Bomberman : InputManager {
-
+public class InputManager_Bomberman : NetworkBehaviour
+{
     float m_playerSpeed = 3.0f;
+    public GameObject bombPrefabReference;
 
     // Use this for initialization
     void Start ()
@@ -12,7 +14,6 @@ public class InputManager_Bomberman : InputManager {
 		
 	}
 
-    // Update is called once per frame
     // Update is called once per frame
     void Update()
     {
@@ -23,35 +24,21 @@ public class InputManager_Bomberman : InputManager {
         float y = Input.GetAxis("Vertical") * Time.deltaTime * m_playerSpeed;
 
         gameObject.transform.Translate(x, y, 0.0f);
-    }
 
-    public override void ProcessHorizontalAxis(float axis)
-    {
-        transform.Translate(axis, 0, 0);
-    }
-
-    public override void ProcessVerticalAxis(float axis)
-    {
-        transform.Translate(0, axis, 0);
-    }
-
-    public override void ProcessShift(bool isDown)
-    {
-        string downUp = isDown ? "DOWN" : "UP";
-        if (isDown)
-            Debug.LogFormat("Shift Button = " + downUp);
-    }
-
-    public override void ProcessSpace(bool isDown)
-    {
-        string downUp = isDown ? "DOWN" : "UP";
-        if (isDown)
+        //check for input
+        bool spaceJustPressed = Input.GetKeyDown(KeyCode.Space);
+        if(spaceJustPressed)
         {
-            Debug.LogFormat("Space Button = " + downUp);
-
-            //SceneController sceneController = GameObject.Find("SceneManager").GetComponent<SceneController>();
-            //sceneController.ChangeSceneName("HotPotato");
+            CmdProcessSpace();
         }
+    }
 
+    [Command]
+    public void CmdProcessSpace()
+    {
+        Debug.LogFormat("Space Button pressed");
+
+        var bomb = (GameObject)Instantiate(bombPrefabReference, gameObject.transform.position, Quaternion.identity);
+        NetworkServer.Spawn(bomb);
     }
 }
